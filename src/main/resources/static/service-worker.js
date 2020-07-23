dd204fc9a396598bc7d339ccd0319ccc771a16de
Aashish Thota <thotaaashish10@gmail.com>
2020-07-23 14:12:37 -0400

    Internet Explorer CDN added

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93653fce9345a480369ca0d1c8249fc0.js"
+  "/precache-manifest.9b9fb39d96f12996fb77716efed2eade.js"
 );
 
 self.addEventListener('message', (event) => {
