b59e2fdbfc2fd966c323624a6795fa4175782618
Aashish Thota <thotaaashish10@gmail.com>
2020-07-23 13:59:09 -0400

    react build is pasted

---
+++ src/main/resources/static/precache-manifest.93653fce9345a480369ca0d1c8249fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959775b0c8eb4992e3796708a777a4b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0057a207fa6d85e1a964",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7c8068d57d7b40f73dfc",
+    "url": "/static/css/main.bf9d1985.chunk.css"
+  },
+  {
+    "revision": "0057a207fa6d85e1a964",
+    "url": "/static/js/2.18e788c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.18e788c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8068d57d7b40f73dfc",
+    "url": "/static/js/main.9e91d866.chunk.js"
+  },
+  {
+    "revision": "250356610c0510c94c48",
+    "url": "/static/js/runtime-main.08d9eb72.js"
+  }
+]);
